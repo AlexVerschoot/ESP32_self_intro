ec31dd1a6f00e84da1b0b478d1c688fc93ccaf12
alex <alexverschoot@gmail.com>
2020-09-29 23:21:19 +0200

    added a way to do button presses

--- main/hello_world_main.c
+++ main/hello_world_main.c
@@ -13,6 +13,11 @@
 #include "esp_system.h"
 #include "esp_spi_flash.h"
 #include "ssd1306.h"
+#include "hal/gpio_types.h"
+#include "esp_err.h"
+#include "driver/gpio.h"
+#include "freertos/task.h"
+#include "freertos/queue.h"
 
 #ifdef CONFIG_IDF_TARGET_ESP32
 #define CHIP_NAME "ESP32"
@@ -21,6 +26,35 @@
 #ifdef CONFIG_IDF_TARGET_ESP32S2BETA
 #define CHIP_NAME "ESP32-S2 Beta"
 #endif
+
+
+static xQueueHandle gpio_evt_queue = NULL;
+static int debounceBool[24];
+
+static void IRAM_ATTR gpio_isr_handler(void* arg)
+{
+    uint32_t gpio_num = (uint32_t) arg;
+    xQueueSendFromISR(gpio_evt_queue, &gpio_num, NULL);
+}
+
+static void gpio_task_example(void* arg)
+{
+    char textChar[24];
+    uint32_t io_num;
+    for(;;) {
+        if(xQueueReceive(gpio_evt_queue, &io_num, portMAX_DELAY)) {
+            if(gpio_get_level(io_num)!=debounceBool[io_num]){
+                printf("GPIO[%d] intr, val: %d\n", io_num, gpio_get_level(io_num));
+                debounceBool[io_num] = gpio_get_level(io_num);
+
+                sprintf(textChar, "E : GPIO[%d] = %d", io_num, gpio_get_level(io_num));
+                ssd1306_printFixed(0,  36, textChar, STYLE_NORMAL);
+            }
+        }
+    }
+}
+
+
 
 void app_main(void)
 {
@@ -35,6 +69,36 @@ void app_main(void)
     ssd1306_clearScreen();
 
     char textChar[36];
+    esp_err_t errorVar;
+
+    
+//using the normal gpio (gpio4) 
+    gpio_config_t io_conf;
+    //interrupt of rising edge
+    io_conf.intr_type = GPIO_PIN_INTR_POSEDGE;
+    //bit mask of the pins, use GPIO4 here
+    io_conf.pin_bit_mask = 4;
+    //set as input mode    
+    io_conf.mode = GPIO_MODE_INPUT;
+    //enable pull-up mode
+    io_conf.pull_up_en = 1;
+    gpio_config(&io_conf);
+
+    //change gpio intrrupt type for one pin
+    gpio_set_intr_type(4, GPIO_INTR_ANYEDGE);
+
+    //create a queue to handle gpio event from isr
+    gpio_evt_queue = xQueueCreate(10, sizeof(uint32_t));
+    //start gpio task
+    xTaskCreate(gpio_task_example, "gpio_task_example", 2048, NULL, 10, NULL);
+
+    //install gpio isr service
+    gpio_install_isr_service(0);
+    //hook isr handler for specific gpio pin
+    gpio_isr_handler_add(4, gpio_isr_handler, (void*) 4);
+
+
+//using the RTC_gpio (rtc_gpio10)
 
     while (true)
     {
@@ -51,9 +115,9 @@ void app_main(void)
 
         for (int i = 1000; i >= 0; i--) {
             sprintf(textChar, "Restarting in %d seconds...", i);
-            printf(textChar);
+            //printf(textChar);
             //ssd1306_clearScreen();
-            ssd1306_printFixed(0,  8, textChar, STYLE_NORMAL);
+            //ssd1306_printFixed(0,  8, textChar, STYLE_NORMAL);
             vTaskDelay(100 / portTICK_PERIOD_MS);
             fflush(stdout);
         }
